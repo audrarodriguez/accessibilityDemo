31d828f9c45f4dce1c8ce2a357d511709f41d2cd
Stephanie Zuniga <szuniga@konradgroup.com>
2022-04-12 17:19:45 -0600

    Add favorites toggle and voice control example

--- Complete/Landmarks/Views/LandmarkList.swift
+++ Complete/Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,32 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                        .font(.title2)
+                }
+                .accessibilityInputLabels([
+                    (showFavoritesOnly ? "Show all" : "Show favorites only"),
+                    "Show favorites"
+                ])
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
