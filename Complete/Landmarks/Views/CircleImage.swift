9291de208d8162b97b2bd29966f9a819866ab691
Stephanie Zuniga <szuniga@konradgroup.com>
2022-04-20 21:42:54 -0600

    add animation on image and condition with reduce motion api

--- Complete/Landmarks/Views/CircleImage.swift
+++ Complete/Landmarks/Views/CircleImage.swift
@@ -8,15 +8,32 @@ A view that clips an image to a circle and adds a stroke and shadow.
 import SwiftUI
 
 struct CircleImage: View {
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @State private var scale = 1.0
+    
     var image: Image
-
+    
     var body: some View {
         image
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
+                    .shadow(radius: 7)
             }
-            .shadow(radius: 7)
+            .scaleEffect(scale)
+            .animation(
+                reduceMotion ? nil : .easeOut(duration: 3),
+                value: scale
+            )
+            .onLongPressGesture (
+                minimumDuration: 0.1,
+                pressing: { isPressing in
+                    guard !reduceMotion else {
+                        return
+                    }
+                    scale = isPressing ? 2.0 : 1.0
+                }
+            ) {}
     }
 }
 
